99c72c82f6bbc36a731216ccaecc546bfd36a87c
tgBTC <tgbtcnews@gmail.com>
2026-01-28 12:59:51 +0600

    Update supply.js

--- api/supply.js
+++ api/supply.js
@@ -2,17 +2,25 @@ export default async function handler(req, res) {
   const MASTER = "kQCxINuwGtspAnynQHKcnhVr2GweYkRZsbKNW0XtaHOAdAAR";
 
   const r = await fetch(
-    `https://testnet.toncenter.com/api/v3/jettons/${MASTER}`,
+    "https://testnet.toncenter.com/api/v2/runGetMethod",
     {
-      headers:{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
         "X-API-Key": process.env.TONCENTER_TESTNET_API_KEY
-      }
+      },
+      body: JSON.stringify({
+        address: MASTER,
+        method: "get_jetton_data",
+        stack: []
+      })
     }
   );
 
   const j = await r.json();
-  res.setHeader("Cache-Control","no-store");
-  res.json({
-    total_supply: j.total_supply
-  });
+
+  // stack[1] = total_supply
+  const totalSupply = j.result.stack[1][1];
+
+  res.json({ total_supply: totalSupply });
 }
